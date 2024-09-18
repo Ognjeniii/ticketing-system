285af959834e70e9da71d46dc49efbdf2791641e
Ognjen Ilić <ognjeni@telekom.rs>
2024-09-18 15:45:05 +0200

    Change pass style

---
+++ ticketing-system/Controllers/Auth/ForgetPassController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ticketing_system.Controllers.Auth
+{
+    public class ForgetPassController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CheckMail()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
