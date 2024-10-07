705f0b185be8d3a796fa53bf87acd12fe700c20e
Ognjen Ilić <ognjeni@telekom.rs>
2024-10-07 11:22:14 +0200

    User service

--- ticketing-system/Models/User/Services/Abstraction/IUserService.cs
+++ ticketing-system/Models/User/Services/Abstraction/IUserService.cs
@@ -2,6 +2,19 @@
 {
     public interface IUserService
     {
-        Task CreateUserAsync(User user);    
+        // Kreiranje novog korisnika
+        Task<User> CreateAsync(User user);
+
+        // Ažuriranje starog korisnika po id-u
+        Task<User> UpdateAsync(int id, User user);
+
+        // Brisanje korisnika po id-u
+        Task DeleteAsync(int id);
+
+        // Dobijanje korisnika na osnovu id-a
+        Task<User> GetByIdAsync(int id);
+
+        // Dobijanje korisnika na osnovu username-a i password-a
+        Task<User> GetByUsernameAndPasswordAsync(string username, string password);
     }
 }
--- ticketing-system/Models/User/Services/Implementation/UserService.cs
+++ ticketing-system/Models/User/Services/Implementation/UserService.cs
@@ -11,9 +11,30 @@ namespace ticketing_system.Models.User.Services.Implementation
         {
             _userRepository = userRepository;
         }
-        public async Task CreateUserAsync(User user)
+
+        public async Task<User> CreateAsync(User user)
         {
-            await _userRepository.CreateAsync(user);
+            return await _userRepository.CreateAsync(user);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _userRepository.DeleteAsync(id);
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _userRepository.GetByIdAsync(id);
+        }
+
+        public async Task<User> GetByUsernameAndPasswordAsync(string username, string password)
+        {
+            return await _userRepository.GetByUsernameAndPasswordAsync(username, password);
+        }
+
+        public async Task<User> UpdateAsync(int id, User user)
+        {
+            return await _userRepository.UpdateAsync(id, user);
         }
     }
 }
