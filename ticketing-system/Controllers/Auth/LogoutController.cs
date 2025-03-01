eb0bb157eb87c2a829ceb886e962c3696aa150f4
ognjeniii <ilicognjen02@gmail.com>
2025-03-01 22:19:53 +0100

    logout function

---
+++ ticketing-system/Controllers/Auth/LogoutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ticketing_system.Controllers.Auth
+{
+    public class LogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            Response.Cookies.Delete("RememberMe");
+            Response.Cookies.Delete("UserId");
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
